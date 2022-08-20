9df8772f1033e775fce6d4440e394be36c173766
Nick Benedetti <nick@coradine.com>
2022-08-19 18:05:09 -0700

    Use env variable inside shell script

--- shell-script.sh
+++ shell-script.sh
@@ -1,6 +1,6 @@
-# echo "${who-to-greet}"
+# Expression syntax does not work in shell commands
+# echo "Hello ${{ inputs.who-to-greet }}"
 
-echo "Hello ${{ inputs.who-to-greet }}"
 echo "arg 1: ${1} arg 2: ${2}"
 echo "$@"
-
+echo "$WHO_GREET"
